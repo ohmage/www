566096c3ffb29d1d8b16531fe6999be97a9fd5cb
Steve Nolen <technolengy@gmail.com>
2015-02-04 14:33:26 -0800

    some more pages and stuff

--- contents/js/scrolling-nav.js
+++ contents/js/scrolling-nav.js
@@ -11,9 +11,11 @@ $(window).scroll(function() {
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+        if($($anchor.attr('data-target')).length > 0) {
+          $('html, body').stop().animate({
+              scrollTop: $($anchor.attr('data-target')).offset().top
+          }, 1500, 'easeInOutExpo');
+          event.preventDefault();
+        }
     });
 });
