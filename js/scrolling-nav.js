ac2fdc3c86fe0eda228c26da1da9d7444da2fcd5
Travis-CI <travis@ohmage.org>
2016-01-27 16:15:23 +0000

    Deployed to Github Pages

---
+++ js/scrolling-nav.js
@@ -0,0 +1,24 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        if($($anchor.attr('data-target')).length > 0) {
+          $('html, body').stop().animate({
+              scrollTop: $($anchor.attr('data-target')).offset().top
+          }, 1500, 'easeInOutExpo');
+          event.preventDefault();
+        }
+    });
+});
+
+
